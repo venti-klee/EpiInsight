1f3438c261969b0cf26470fafff8fcd5a7ce31ad
xi <1213790684@qq.com>
2022-09-07 19:05:06 +0800

    添加历史折线图

--- src/components/EchartDialog.vue
+++ src/components/EchartDialog.vue
@@ -11,6 +11,7 @@
 
     <div class="echartDiv">
       <div id="histogramDiv"></div>
+      <div id="historyLineDiv"></div>
       <p>国内累计总数：{{ props.daily.addcon }}</p>
       <p>国内死亡总数：{{ props.daily.adddeath }}</p>
       <p>国内治愈总数：{{ props.daily.addcure }}</p>
@@ -28,22 +29,31 @@ let props: any = defineProps({
     type: Boolean,
     default: false
   },
+  //国家排名
   sortList: {
     type: Array,
     default: [],
   },
+  //国内基础数据
   daily: {
     type: Object,
     default: {}
   },
+  //境外输入省份前10
   jwsrTop: {
     type: Array,
     default: [],
   },
+  //国内历史列表
+  historylist: {
+    type: Array,
+    default: [],
+  }
 })
 
 let isEchart = ref(false),
-  histogramChart: any = null;//柱状图
+  histogramChart: any = null,//柱状图
+  historyLineChart: any = null;//历史折线图
 
 //watch可监听指定属性watchEffect不能
 watch(
@@ -51,7 +61,7 @@ watch(
   async (val) => {
     if (val) {
       await (isEchart.value = val);
-      initChart(props.sortList.slice(0, 5)); //取出前五个，初始化图表  
+      initChart();
     }
   },
 )
@@ -65,7 +75,13 @@ function handleClose() {
 };
 
 //初始化图表
-function initChart(list: any) {
+function initChart() {
+  histogramChartFun(props.sortList.slice(0, 5)); //取出前五个，绘制国家排名柱状图
+  historyLineChartFun(props.historylist);//绘制历史折线图
+}
+
+//国家排名柱状图
+function histogramChartFun(list: any) {
   let chartDom = document.getElementById("histogramDiv");
   (histogramChart) && (histogramChart.dispose());//销毁实例
   histogramChart = echarts.init(chartDom);
@@ -129,6 +145,108 @@ function initChart(list: any) {
   option && histogramChart.setOption(option);
 }
 
+//历史折线图
+function historyLineChartFun(list: any) {
+  let lineData = JSON.parse(JSON.stringify(list));
+  lineData.reverse();
+  let chartDom = document.getElementById("historyLineDiv");
+  (historyLineChart) && (historyLineChart.dispose());//销毁实例
+  historyLineChart = echarts.init(chartDom);
+  let option: any = {
+    backgroundColor: "#000000aa",
+    title: {
+      text: '国内历史数据',
+      left: "center",
+      textStyle: {
+        color: "#fff",
+      },
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    grid: {
+      // left: '5%',
+      // right: '15%',
+      // bottom: '10%'
+    },
+    xAxis: {
+      data: lineData.map(function (item: any) {
+        return item.ymd;
+      }),
+    },
+    yAxis: {
+      textStyle: {
+        color: "#fff",
+      },
+    },
+    // toolbox: {
+    //   right: 10,
+    //   feature: {
+    //     dataZoom: {
+    //       yAxisIndex: 'none'
+    //     },
+    //     restore: {},
+    //     saveAsImage: {}
+    //   }
+    // },
+    dataZoom: [
+      {
+        startValue: '2.10',
+      },
+      {
+        type: 'inside'
+      }
+    ],
+    series: {
+      name: '累计数',
+      type: 'line',
+      lineStyle: {
+        color: '#f00'
+      },
+      data: lineData.map(function (item: any) {
+        return item.cn_conNum;
+      }),
+      zlevel: 1,
+      z: 1,
+      markLine: {
+        lineStyle: {
+          color: '#fff'
+        },
+        data: [
+          {
+            yAxis: 100000
+          },
+          {
+            yAxis: 500000
+          },
+          {
+            yAxis: 1000000
+          },
+          {
+            yAxis: 2000000
+          },
+          {
+            yAxis: 3000000
+          },
+          {
+            yAxis: 4000000
+          },
+          {
+            yAxis: 5000000
+          },
+          {
+            yAxis: 6000000
+          },
+          {
+            yAxis: 7000000
+          },
+        ]
+      },
+    }
+  };
+  option && historyLineChart.setOption(option);
+};
+
 </script>
 <style scoped lang='scss'>
 .my-header {
@@ -160,6 +278,11 @@ function initChart(list: any) {
   #histogramDiv {
     height: 200px;
     width: 400px;
+  }
+
+  #historyLineDiv {
+    height: 400px;
+    width: 800px;
   }
 }
 </style>
