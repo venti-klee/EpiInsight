560bfcbafc9e631b7ddd832e7bf54b1965441587
xi <1213790684@qq.com>
2022-10-16 23:47:35 +0800

    添加xlsx的file-saver下载方式

--- src/utils/xlsxUtils.ts
+++ src/utils/xlsxUtils.ts
@@ -1,4 +1,5 @@
 import * as XLSX from "xlsx";
+import { saveAs } from 'file-saver'
 
 //入参示例
 let eg = {
@@ -11,6 +12,30 @@ let eg = {
         { name: "日本", population: "35", test: "t3" }
     ]//对象数组
 }
+
+function workbook2blob(workbook: any) {
+    // 生成excel的配置项
+    let wopts: any = {
+        // 要生成的文件类型
+        bookType: "xlsx",
+        // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+        bookSST: false,
+        type: "binary"
+    };
+    var wbout = XLSX.write(workbook, wopts);
+    // 将字符串转ArrayBuffer
+    function s2ab(s: any) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
+        return buf;
+    }
+    let buf = s2ab(wbout);
+    var blob = new Blob([buf], {
+        type: "application/octet-stream"
+    });
+    return blob;
+};
 
 //导出数据表格
 export default async function downloadXlsx(tabObj: any) {
@@ -29,5 +54,11 @@ export default async function downloadXlsx(tabObj: any) {
     workSheet = XLSX.utils.aoa_to_sheet(aoaList); //将列表数据添加到工作表
     let workBook = XLSX.utils.book_new(); //创建一个工作薄
     XLSX.utils.book_append_sheet(workBook, workSheet, "1"); //将工作表添加到工作薄中
-    await XLSX.writeFile(workBook, tabObj.fileName + ".xlsx"); //写入文件，下载工作薄
+    //下载方式一：用XLSX的writeFile，写入文件，下载工作薄
+    // await XLSX.writeFile(workBook, tabObj.fileName + ".xlsx"); 
+    //下载方式二:利用file-saver的saveAs下载Blob
+    saveAs(
+        new Blob([workbook2blob(workBook)], { type: "application/octet-stream" }),
+        tabObj.fileName + ".xlsx"
+    );
 };
